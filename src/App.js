c97e26e3424b532e5e8c57ef2f8496ede2f667bd
Clément Hangard <clement.hangard31@gmail.com>
2021-11-04 16:59:36 +0100

    Integration of Truffle files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import getWeb3 from "./getWeb3";
+import { useEffect, useState } from "react";
+import Migrations from "./abis/Migrations.json";
 
 function App() {
+  const [address, setAddress] = useState("");
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    _init();
+  }, []);
+
+  const _init = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Migrations.networks[networkId];
+      const instance = new web3.eth.Contract(
+        Migrations.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      setAddress(accounts[0]);
+      setContract(instance);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Your address : {address}</div>
     </div>
   );
 }
